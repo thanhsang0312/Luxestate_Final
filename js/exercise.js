c3261138c0f58ca1dee0a3a94948ce19e778e05d
Sang Cao <thanhsang0312@gmail.com>
2023-11-30 23:52:53 +0700

    final update

---
+++ js/exercise.js
@@ -0,0 +1,78 @@
+// **Exercise 1:
+// var my_numb = prompt("Please enter number: ");
+// if (my_numb % 2 === 0) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+// ----------------------------------------------
+// **Exercise 2:
+// var my_numb = prompt("Please enter number: ");
+// if (my_numb % 3 === 0) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+// ----------------------------------------------
+// **Exercise 3:
+// var a = 4;
+// var b = 12;
+// console.log(`a = ${a}`);
+// console.log(`b = ${b}`);
+// console.log(`a + b = ${a + b}`);
+// console.log(`a % b = ${a % b}`);
+// console.log(`a / b = ${a / b}`);
+// ----------------------------------------------
+// **Exercise 4:
+// var fname = 'Thanh Sang';
+// var lname = 'Cao';
+// console.log(lname, fname);
+// ----------------------------------------------
+// **Exercise 5:
+// var a = 5;
+// var b = 5;
+// if (a > b) {
+//     console.log(1);
+// }
+// else if (a < b) {
+//     console.log(-1);
+// }
+// else {
+//     console.log(0);
+// }
+// ----------------------------------------------
+// **Exercise 6:
+// var my_string = "happy new year";
+// (a)
+// var i = 0;
+// for (i; i < my_string.length; i++) {
+// }
+// console.log(`Tổng số ký tự có trong chuỗi: ${i}`);
+// // (b)
+// console.log(`Index ký tự "y" tính từ đầu: ${my_string.indexOf("y")}`);
+// // (c)
+// console.log(`Index ký tự "y" tính từ cuối: ${my_string.lastIndexOf("y")}`);
+// // (d)
+// console.log(`Replace "happy": ${my_string.replace("happy", "sad")}`);
+// ----------------------------------------------
+// **Exercise 7:
+// function myFunction() {
+//     let my_points = prompt("Enter your points: ");
+//     if (my_points > 8) {
+//         console.log("Excellence!");
+//     }
+//     else if (my_points > 7) {
+//         console.log("Good!");
+//     }
+//     else if (my_points > 4) {
+//         console.log("Not Good!");
+//     }
+//     else {
+//         console.log("Bad!!!");
+//     }
+// }
+// var points = myFunction();
+// ----------------------------------------------
+// **Exercise 8:
---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Scroll to section
+function scrollToCompany() {
+    const abouts = document.getElementById("abouts");
+    abouts.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+function scrollToApartments() {
+    const apartments = document.getElementById("apartments");
+    apartments.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+function scrollToWorks(){
+    const works = document.getElementById("works");
+    works.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+function scrollToAgents() {
+    const agents = document.getElementById("agents");
+    agents.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+function scrollToLocation() {
+    const contact = document.getElementById("contact");
+    contact.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+function scrollToGIT() {
+    const git = document.getElementById("git");
+    git.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+// End Scroll to section
+
+// Scroll to top button
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+}
+// End Scroll to top button
